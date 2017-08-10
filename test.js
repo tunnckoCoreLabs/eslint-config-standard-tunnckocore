a5a3f1d3b16267f0c79aea33c812d3785358ec2e
tunnckoCore <mameto2011@gmail.com>
2017-08-10 05:30:59 +0300

    init

---
+++ test.js
@@ -0,0 +1,12 @@
+/*!
+ * hela <https://github.com/tunnckoCore/hela>
+ *
+ * Copyright (c) 2017 Charlike Mike Reagent <open.source.charlike@gmail.com> (https://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+const test = require('mukla') // eslint-disable-line
+
+test('foo bar', () => {
+  console.log('ok')
+})
